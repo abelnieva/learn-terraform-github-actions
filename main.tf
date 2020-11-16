b4a73aa3167fd4f96775461f8f3561f1b9fffcf3
Abel Alejandro Nieva <abel@abelnieva.ar>
2020-11-16 15:34:23 +0100

    skpping creds

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region                      = "us-west-2"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
